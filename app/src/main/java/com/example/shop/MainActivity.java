80fe74ea1954905763d2621c8667d94114f501fd
Szekeres Tibor <25374@4ig.hu>
2022-05-07 13:33:01 +0200

    Login + Registration combined

--- app/src/main/java/com/example/shop/MainActivity.java
+++ app/src/main/java/com/example/shop/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.shop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,10 +11,13 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getName();
+    private static final String PREF_KEY = MainActivity.class.getPackage().toString();
     private static final int SECRET_KEY = 99;
 
     EditText userNameET;
     EditText passwordET;
+
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
 
         userNameET = findViewById(R.id.editTextUserName);
         passwordET = findViewById(R.id.editTextPassword);
+
+        preferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE);
+
+        Log.i(LOG_TAG, "onCreate");
     }
 
     public void login(View view) {
@@ -33,7 +41,50 @@ public class MainActivity extends AppCompatActivity {
 
     public void registration(View view) {
         Intent intent = new Intent(this, RegistrationActivity.class);
-        intent.putExtra("SECRET_KEY", 99);
+        intent.putExtra("SECRET_KEY", SECRET_KEY);
         startActivity(intent);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("userName", userNameET.getText().toString());
+        editor.putString("password", passwordET.getText().toString());
+        editor.apply();
+
+        Log.i(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(LOG_TAG, "onRestart");
+    }
+
 }
--- app/src/main/java/com/example/shop/RegistrationActivity.java
+++ app/src/main/java/com/example/shop/RegistrationActivity.java
@@ -2,9 +2,22 @@ package com.example.shop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class RegistrationActivity extends AppCompatActivity {
+    private static final String LOG_TAG = RegistrationActivity.class.getName();
+    private static final String PREF_KEY = RegistrationActivity.class.getPackage().toString();
+
+    EditText userNameET;
+    EditText userEmailET;
+    EditText passwordET;
+    EditText passwordAgainET;
+
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +29,74 @@ public class RegistrationActivity extends AppCompatActivity {
         if(secret_key != 99) {
             finish();
         }
+
+        userNameET = findViewById(R.id.userNameEditText);
+        userEmailET = findViewById(R.id.userEmailEditText);
+        passwordET = findViewById(R.id.passwordEditText);
+        passwordAgainET = findViewById(R.id.passwordAgainEditText);
+
+        preferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE);
+        String userName = preferences.getString("userName", "");
+        String password = preferences.getString("password", "");
+
+        userNameET.setText(userName);
+        passwordET.setText(password);
+        passwordAgainET.setText(password);
+
+        Log.i(LOG_TAG, "onCreate");
+    }
+
+    public void register(View view) {
+        String userName = userNameET.getText().toString();
+        String email = userEmailET.getText().toString();
+        String password = passwordET.getText().toString();
+        String passwordAgain = passwordAgainET.getText().toString();
+
+        if(!password.equals(passwordAgain)) {
+            Log.e(LOG_TAG, "Passwords do not match!");
+            return;
+        }
+
+        Log.i(LOG_TAG, "Regisztrált:" + userName + ", email: " + email);
+    }
+
+    public void cancel(View view) {
+        finish();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(LOG_TAG, "onRestart");
     }
 }
