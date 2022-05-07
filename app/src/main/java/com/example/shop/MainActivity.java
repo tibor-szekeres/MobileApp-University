711dc60ff956bb32081a975a1284fc7baf5f4485
Szekeres Tibor <25374@4ig.hu>
2022-05-07 12:02:15 +0200

    Login site

--- app/src/main/java/com/example/shop/MainActivity.java
+++ app/src/main/java/com/example/shop/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.shop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getName();
+
+    EditText userNameET;
+    EditText passwordET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userNameET = findViewById(R.id.editTextUserName);
+        passwordET = findViewById(R.id.editPassword);
+    }
+
+    public void login(View view) {
+        String userName = userNameET.getText().toString();
+        String password = passwordET.getText().toString();
+
+        Log.i(LOG_TAG, "Bejelentkezett:" + userName + ", jelszó: " + password);
     }
 }
